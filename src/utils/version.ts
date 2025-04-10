fc8bbde13ecf83df0393b2ac494b50938479888e
Deepak Jois <deepak.jois@gmail.com>
2025-04-10 12:39:21 +0530

    Use pre-build script to generate version.ts

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,9 +1,2 @@
-// Read package.json version at runtime instead of import
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-const packageJson = JSON.parse(readFileSync(join(__dirname, '..', '..', 'package.json'), 'utf8'));
-
-export const version = packageJson.version as string;
+// Auto-generated file, do not edit
+export const version = '1.2.1';
