dca52f4cddacc53c264295d4c3af852f164d6a05
Stanislav Lysenko <stanislav.lysenko0912@gmail.com>
2025-04-13 15:03:51 +0200

    Fix formatting and update instructions text in comments

--- src/clients.ts
+++ src/clients.ts
@@ -1,15 +1,15 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { TodoistClient } from "./utils/TodoistClient.js";
-import { config, version } from "./utils/helpers.js";
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { TodoistClient } from './utils/TodoistClient.js';
+import { config, version } from './utils/helpers.js';
 
 export const todoistApi = new TodoistClient(config.API_KEY);
 
 export const server = new McpServer(
     {
         name: 'todoist-mcp',
-        version
+        version,
     },
     {
-        instructions: "You this server to interact with Todoist API"
+        instructions: 'Use this server to interact with Todoist API',
     }
 );
